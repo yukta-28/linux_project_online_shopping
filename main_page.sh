11b8c6fee4ea55fd6fdc89b28d25a62bf993d1ee
Yoniabeje <yonatanabeje@gmail.com>
2021-03-29 16:23:51 -0400

    intial commit

---
+++ main_page.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+
+figlet -c Esport Arsenal 
+
+echo "Computers & Electronics Online Store"
+echo "************************************"
+
+echo "PRODCUTS"
+echo "1 CPU"
+echo "    1. AMD CPU------\$300.00"
+echo "    2. Intel CPU----\$250.00"
+
+echo "2 GPU"
+echo "    1. AMD GPU------\$100.00"
+echo "    2. NVidia GPU---\$200.00"   
