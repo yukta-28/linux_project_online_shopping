e3652da1dab00bd8415f5624d23e559a897eaca7
Roger Kidd <80714082+RogerKidd@users.noreply.github.com>
2021-04-12 14:29:28 -0400

    Delete custompc1header.sh

--- custompc1header.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-echo "WARNING: THIS IS A PROTOTYPE SCRIPT. DON'T ENTER REAL ADDRESS/PAYMENT INFO"
-figlet -f slant "Esport Arsenal"
-echo "* * * * * * * * * * * Hyper speed tournament PC shop * * * * * * * * * * *"
-echo ""
-echo ""
