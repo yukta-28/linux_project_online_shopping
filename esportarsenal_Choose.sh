bba37dfdc8fa5fd783553baa8646010ddf4919c1
Roger Kidd <80714082+RogerKidd@users.noreply.github.com>
2021-04-15 14:45:40 -0400

    Add files via upload
    Updated _Choose

--- esportarsenal_Choose.sh
+++ esportarsenal_Choose.sh
@@ -40,10 +40,10 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${Mobo[0]}, \$${Mobo_cost[0]}"
-echo "1. ${Mobo[1]}, \$${Mobo_cost[1]}"
-echo "2. ${Mobo[2]}, \$${Mobo_cost[2]}"
-echo "3. ${Mobo[3]}, \$${Mobo_cost[3]}"
+echo "1. ${Mobo[0]}, \$${Mobo_cost[0]}"
+echo "2. ${Mobo[1]}, \$${Mobo_cost[1]}"
+echo "3. ${Mobo[2]}, \$${Mobo_cost[2]}"
+echo "4. ${Mobo[3]}, \$${Mobo_cost[3]}"
 read -p "Choose your motherboard: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${Mobo[Indexer]}
@@ -58,11 +58,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${GPU[0]}, \$${GPU_cost[0]}"
-echo "1. ${GPU[1]}, \$${GPU_cost[1]}"
-echo "2. ${GPU[2]}, \$${GPU_cost[2]}"
-echo "3. ${GPU[3]}, \$${GPU_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${GPU[0]}, \$${GPU_cost[0]}"
+echo "2. ${GPU[1]}, \$${GPU_cost[1]}"
+echo "3. ${GPU[2]}, \$${GPU_cost[2]}"
+echo "4. ${GPU[3]}, \$${GPU_cost[3]}"
+read -p "Choose your graphics card: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${GPU[Indexer]}
 Part_cost_array[Category]=${GPU_cost[Indexer]}
@@ -76,11 +76,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${RAM[0]}, \$${RAM_cost[0]}"
-echo "1. ${RAM[1]}, \$${RAM_cost[1]}"
-echo "2. ${RAM[2]}, \$${RAM_cost[2]}"
-echo "3. ${RAM[3]}, \$${RAM_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${RAM[0]}, \$${RAM_cost[0]}"
+echo "2. ${RAM[1]}, \$${RAM_cost[1]}"
+echo "3. ${RAM[2]}, \$${RAM_cost[2]}"
+echo "4. ${RAM[3]}, \$${RAM_cost[3]}"
+read -p "Choose your RAM: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${RAM[Indexer]}
 Part_cost_array[Category]=${RAM_cost[Indexer]}
@@ -94,11 +94,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${PSU[0]}, \$${PSU_cost[0]}"
-echo "1. ${PSU[1]}, \$${PSU_cost[1]}"
-echo "2. ${PSU[2]}, \$${PSU_cost[2]}"
-echo "3. ${PSU[3]}, \$${PSU_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${PSU[0]}, \$${PSU_cost[0]}"
+echo "2. ${PSU[1]}, \$${PSU_cost[1]}"
+echo "3. ${PSU[2]}, \$${PSU_cost[2]}"
+echo "4. ${PSU[3]}, \$${PSU_cost[3]}"
+read -p "Choose your power supply: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${PSU[Indexer]}
 Part_cost_array[Category]=${PSU_cost[Indexer]}
@@ -112,11 +112,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${Case[0]}, \$${Case_cost[0]}"
-echo "1. ${Case[1]}, \$${Case_cost[1]}"
-echo "2. ${Case[2]}, \$${Case_cost[2]}"
-echo "3. ${Case[3]}, \$${Case_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${Case[0]}, \$${Case_cost[0]}"
+echo "2. ${Case[1]}, \$${Case_cost[1]}"
+echo "3. ${Case[2]}, \$${Case_cost[2]}"
+echo "4. ${Case[3]}, \$${Case_cost[3]}"
+read -p "Choose your case: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${Case[Indexer]}
 Part_cost_array[Category]=${Case_cost[Indexer]}
@@ -130,11 +130,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${SSD[0]}, \$${SSD_cost[0]}"
-echo "1. ${SSD[1]}, \$${SSD_cost[1]}"
-echo "2. ${SSD[2]}, \$${SSD_cost[2]}"
-echo "3. ${SSD[3]}, \$${SSD_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${SSD[0]}, \$${SSD_cost[0]}"
+echo "2. ${SSD[1]}, \$${SSD_cost[1]}"
+echo "3. ${SSD[2]}, \$${SSD_cost[2]}"
+echo "4. ${SSD[3]}, \$${SSD_cost[3]}"
+read -p "Choose your solid state drive: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${SSD[Indexer]}
 Part_cost_array[Category]=${SSD_cost[Indexer]}
@@ -148,11 +148,11 @@ while [[ $Indexer != 0 && $Indexer != 1 && $Indexer != 2 && $Indexer != 3 ]]
 do
 ./esportarsenal_Header.sh
 Subheader
-echo "0. ${OS[0]}, \$${OS_cost[0]}"
-echo "1. ${OS[1]}, \$${OS_cost[1]}"
-echo "2. ${OS[2]}, \$${OS_cost[2]}"
-echo "3. ${OS[3]}, \$${OS_cost[3]}"
-read -p "Choose your motherboard: " Indexer
+echo "1. ${OS[0]}, \$${OS_cost[0]}"
+echo "2. ${OS[1]}, \$${OS_cost[1]}"
+echo "3. ${OS[2]}, \$${OS_cost[2]}"
+echo "4. ${OS[3]}, \$${OS_cost[3]}"
+read -p "Choose your operating system: " Indexer
 ((Indexer=Indexer-1))
 Part_name_array[Category]=${OS[Indexer]}
 Part_cost_array[Category]=${OS_cost[Indexer]}
